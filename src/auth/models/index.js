20fc03d3e579b4e3a778265e448227781b922d44
MichaelCampbell2020 <cam3000@gmail.com>
2022-03-29 16:48:31 -0700

    ready to take down some tests

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -5,14 +5,14 @@ const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users.js');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
-
+// use sql lite memory for database url
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   dialectOptions: {
     ssl: true,
     rejectUnauthorized: false,
   }
 } : {};
-
+console.log('---*----',DATABASE_URL);
 const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
 
 module.exports = {
