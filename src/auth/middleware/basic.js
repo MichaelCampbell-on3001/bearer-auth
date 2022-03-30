978a73d27a932e32aeb1ed1e62c8912a6bd0a541
MichaelCampbell2020 <cam3000@gmail.com>
2022-03-29 21:47:52 -0700

    no tests pass

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -5,15 +5,21 @@ const { user } = require('../models/index.js')
 
 module.exports = async (req, res, next) => {
 
-  if (!req.headers.authorization) { return _authError(); }
-
-  let basic = req.headers.authorization;
-  let [username, pass] = base64.decode(basic).split(':');
+  // if (!req.headers.authorization) { return _authError(); }
 
   try {
-    req.user = await user.authenticateBasic(username, pass)
+
+  let basic = req.headers.authorization;
+  let authString = basic.split(' ')[1];
+  let [username, password] = base64.decode(authString).split(':');
+
+    req.user = await user.authenticateBasic(username, password);
+
+    if (!req.user) throw new Error;
+
     next();
   } catch (e) {
+    console.error(e);
     res.status(403).send('Invalid Login');
   }
 
